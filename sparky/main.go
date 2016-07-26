dbf2813ce948468c709e46cd9d8530903fc7729f
Mr. Is <isaac.ardis@gmail.com>
2016-07-26 16:41:42 -0400

    dont capitalize things dont need capitalizing

--- sparky/main.go
+++ sparky/main.go
@@ -10,18 +10,18 @@ import (
 	"github.com/nlopes/slack"
 )
 
-func ListenForHumanWeakness() {
+func listenForHumanWeakness() {
 	bot := slackbot.New(os.Getenv("SLACK_API_TOKEN"))
-	bot.Hear("(fail|tried|err|break|broke|won(|')t|can(|')t)").MessageHandler(HumanFailureHandler)
+	bot.Hear("(fail|tried|err|break|broke|won(|')t|can(|')t)").MessageHandler(humanFailureHandler)
 	bot.Run()
 }
 
-func HumanFailureHandler(ctx context.Context, bot *slackbot.Bot, msg *slack.MessageEvent) {
+func humanFailureHandler(ctx context.Context, bot *slackbot.Bot, msg *slack.MessageEvent) {
 	bot.Reply(msg, fmt.Sprintf("Suckas! For robots, perfection is effortless."), slackbot.WithTyping)
 }
 
 func main() {
 	fmt.Println("I am sparky and I am sparking.")
 	fmt.Println("My slack token is", os.Getenv("SPARKY_SLACK_API_TOKEN"))
-	ListenForHumanWeakness()
+	listenForHumanWeakness()
 }
